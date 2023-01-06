21dfbc1333e3513a78a0b0bb36e8945e544d64ae
Web3 Jobs <5325909+AndreyAzimov@users.noreply.github.com>
2023-01-06 23:04:51 +0000

    Update google_sheet.rb

--- google_sheet.rb
+++ google_sheet.rb
@@ -7,27 +7,20 @@ class GoogleSheet
   # Copy that link that should look like this: https://docs.google.com/spreadsheets/d/e/2PACX-1vRfSXjhd7hDjagB7sX9CX1K7lDWJ6djduoILKoyZAYTWihbC8W336ZH-MtXIPNsulhy6sga/pubhtml?gid=0&single=true
   #
   # Usage GoogleSheet.get "https://docs.google.com/spreadsheets/d/e/2PACX-1vRfSXjhd7hDjagB7sX9CX1K7lDWJ6djduoILKoyZAYTWihbC8W336ZH-MtXIPNsulhy6sga/pubhtml?gid=0&single=true"
-  
+
   require 'open-uri'
   require 'csv'
 
   def self.get url
 
-    csv_file = "public/csv/comapnies.csv"
+    csv_file = "#{Rails.root}/public/data.csv"
 
-    open(url) {|f|
-      File.open(csv_file,"wb") do |file|
-        file.write f.read
-      end
-    }
-
-    csv_file = "#{Rails.root}/public/csv/comapnies.csv"
+    open(url) {|f| File.open(csv_file,"wb") {|file| file.write f.read}}
 
     parsed_data = []
 
     CSV.open(csv_file, 'r:bom|utf-8', headers: true).each do |row|
-      data = row.to_hash
-      parsed_data << data
+      parsed_data << row.to_hash
     end
 
     return parsed_data

b702b0b86c1d309c4a09317ddb43b9dd3546fd0e
Web3 Jobs <5325909+AndreyAzimov@users.noreply.github.com>
2023-01-07 00:13:15 +0000

    Update google_sheet.rb

--- google_sheet.rb
+++ google_sheet.rb
@@ -2,17 +2,17 @@ class GoogleSheet
   require 'open-uri'
   require 'csv'
 
-  # https://github.com/AndreyAzimov/ruby-on-rails-google-sheet-csv/blob/main/README.md
-
   def self.get url
 
     csv_file = "#{Rails.root}/public/data.csv"
-
+    
+    # save csv file from url
     open(url) {|f| File.open(csv_file,"wb") {|file| file.write f.read}}
 
     data = []
-
-    CSV.open(csv_file, 'r:bom|utf-8', headers: true).each{|row| data << row.to_hash}
+  
+    # parse each csv row
+    CSV.open(csv_file, headers: true).each{|row| data << row.to_hash}
 
     return data
   end
